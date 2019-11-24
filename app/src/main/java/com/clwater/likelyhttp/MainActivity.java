83e7aa1d8b61a0ac15c73e3337420bdd8873a38c
clwater <gengzhibo@gengzhibo.net>
2019-11-24 23:53:52 +0800

    fea: import library

--- app/src/main/java/com/clwater/likelyhttp/MainActivity.java
+++ app/src/main/java/com/clwater/likelyhttp/MainActivity.java
@@ -2,6 +2,9 @@ package com.clwater.likelyhttp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.clwater.library.Test;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                test();
+            }
+        });
+    }
+
+    private void test() {
+        Test.test(this);
     }
 }
